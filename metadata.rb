44a5a5e0eae4eb9d1e53da88799e7546f86a64fb
Chris Escalante <chrise@ku.edu>
2014-11-06 16:09:02 -0600

    Update to read me and metadata file for php5.6

--- metadata.rb
+++ metadata.rb
@@ -2,12 +2,13 @@ name             'b9php'
 maintainer       'Chris Escalante'
 maintainer_email 'chris@beyond9.co'
 license          'All rights reserved'
-description      'Installs/Configures PHP 5.5 from Remi'
-version          '0.0.1'
+description      'Installs/Configures PHP 5.6 from Remi'
+version          '0.0.2'
 
-recipe "b9php", "Install php 5.5"
+recipe "b9php", "Install php 5.6"
 recipe "b9php::remi", "Installs REMI repo"
 recipe "b9php::remi55", "Installs REMI55 repo"
+recipe "b9php::remi56", "Installs REMI55 repo"
 recipe "b9php::pear", "Installs pear extensions"
 recipe "b9php::oracle", "Installs pear extensions"
 
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # b9php
 
-Installs PHP 5.5 from the remi repo. Also installs a list of extensions. 
+Installs PHP 5.6 from the remi repo. Also installs a list of extensions. 
 
 See the default recipe for the complete list. 
 
